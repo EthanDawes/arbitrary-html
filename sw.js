41fbf198e69b7e5a5134d6bef1e67eed75f4a798
Glitch (hello-webpage) <none>
2020-09-18 19:59:15 +0000

    ⚡️🛳 Checkpoint ./sw.js:4291982/24

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@ self.addEventListener('fetch', (event) => {
   
   if (event.request.mode === 'navigate') {
     event.respondWith(
-      new Response((url.hash == '') ? "Insert the HTML after 'https://arbitrary-html.glitch.me/#'" : decodeURI(url.hash.substring(1)), {headers: {'content-type': 'text/html'}}
+      new Response((url.hash == '') ? 'Insert the HTML after "https://arbitrary-html.glitch.me/#"' : decodeURI(url.hash.substring(1)), {headers: {'content-type': 'text/html'}}
     ));
   }
 });
