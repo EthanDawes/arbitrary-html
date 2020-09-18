88ea4d0b9f81f7b256bc1a1b2b74bbb89895798f
Glitch (hello-webpage) <none>
2020-09-18 17:34:11 +0000

    💶💳 Checkpoint ./sw.js:4291982/477 ./index.html:4291982/2

--- sw.js
+++ sw.js
@@ -3,9 +3,6 @@
 // Resource https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 var workbox;  // needed to prevent "workbox is undefined" glitch.com warning
-const staticResources = [
-  
-];
 
 
 workbox.setConfig({
@@ -19,20 +16,12 @@ self.addEventListener('fetch', (event) => {
   const url = new URL(event.request.url);
   
   if (event.request.mode === 'navigate') {
-    event.respondWith(new Response(decodeURI(url.pathname.substring(1)), {
-        headers: {'content-type': 'text/html'},
-      }));
+    event.respondWith(
+      new Response(decodeURI(url.pathname.substring(1)), {headers: {'content-type': 'text/html'}}
+    ));
   }
 });
 
 // This immediately deploys the service worker w/o requiring a refresh
 workbox.core.skipWaiting();
 workbox.core.clientsClaim();
-
-/*self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(workbox.core.cacheNames.runtime).then(function(cache) {
-      return cache.addAll(staticResources);
-    })
-  );
-});*/
